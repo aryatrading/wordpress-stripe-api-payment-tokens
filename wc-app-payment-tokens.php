3e4ea8a0067a04d4933ecf9860be19700d2b847a
Oussama Harzallah <oharzallah@greenbull.group>
2019-12-02 11:48:22 +0100

    Fix filter

--- wc-app-payment-tokens.php
+++ wc-app-payment-tokens.php
@@ -64,6 +64,18 @@ function wc_app_payment_tokens_load()
 add_action('plugins_loaded', 'wc_app_payment_tokens_load');
 
 
+/**
+ *
+ * Fix Subscription Renewal :
+ *  => In the `_stripe_source_id` meta data AND in the table `woocommerce_payment_tokens` we store the payment method ID ('pm_xxx...'), not the Source ID like Woocommerce do : 'src_xxx...'
+ *  => In the case of a payment intent we have to change the object sended to stripe and add the correct attribute
+ *  => To do this :
+ *      1/ we use the filter `wc_stripe_generate_payment_request` to add an attribute payment_method
+ *      2/ With the filter `woocommerce_stripe_request_body` before sending the request if we have a `payment_method` we remove `source` attribute
+ *      3/ We must make a manual change in  the file  `woocommerce-gateway-stripe/includes/abstracts/abstract-wc-stripe-payment-gateway.php` > create_and_confirm_intent_for_off_session()
+ *          => Add in line 1369 : `if (isset($full_request['payment_method'])) $request['payment_method'] = $full_request['payment_method'];`
+ */
+
 add_filter('wc_stripe_generate_payment_request', 'filter_wc_stripe_generate_payment_request_payment_method', 3, 10);
 function filter_wc_stripe_generate_payment_request_payment_method($post_data, $order, $source)
 {
@@ -77,7 +89,12 @@ function filter_wc_stripe_generate_payment_request_payment_method($post_data, $o
 add_filter('woocommerce_stripe_request_body', 'filter_woocommerce_stripe_request_body_payment_method', 3, 10);
 function filter_woocommerce_stripe_request_body_payment_method($request, $api)
 {
-    if ($api === "payment_intents" && $request['payment_method'] && substr($request['payment_method'], 0, 2) === "pm") {
+    /*if ($api === "payment_intents" && $request['payment_method'] && substr($request['payment_method'], 0, 2) === "pm") {
+        unset($request['source']);
+    }*/
+
+    if ($api === "payment_intents" && $request['source'] && substr($request['source'], 0, 2) === "pm") {
+        $request['payment_method'] = $request['source'];
         unset($request['source']);
     }
 
